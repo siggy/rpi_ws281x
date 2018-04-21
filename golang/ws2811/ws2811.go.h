f0614f8b2ecf594d72ddf3f37df711f7ad31dc88
Andrew Seigner <andrew@sig.gy>
2018-04-21 02:11:34 -0700

    multi-channel support

--- golang/ws2811/ws2811.go.h
+++ golang/ws2811/ws2811.go.h
@@ -31,7 +31,7 @@
 
 ws2811_t ledstring = {
    .freq = 800000,
-   .dmanum = 5,
+   .dmanum = 10,
    .channel = {
 	   [0] = {
 		   .gpionum = 18,
@@ -50,8 +50,8 @@ ws2811_t ledstring = {
    },
 };
 
-void ws2811_set_led(ws2811_t *ws2811, int index, uint32_t value) {
-	ws2811->channel[0].leds[index] = value;
+void ws2811_set_led(ws2811_t *ws2811, int channel, int index, uint32_t value) {
+	ws2811->channel[channel].leds[index] = value;
 }
 
 void ws2811_clear(ws2811_t *ws2811) {
@@ -61,6 +61,6 @@ void ws2811_clear(ws2811_t *ws2811) {
 	}
 }
 
-void ws2811_set_bitmap(ws2811_t *ws2811, void* a, int len) {
-	memcpy(ws2811->channel[0].leds, a, len);
+void ws2811_set_bitmap(ws2811_t *ws2811, int channel, void* a, int len) {
+	memcpy(ws2811->channel[channel].leds, a, len);
 }
